f77621952279016c50541ce2e90b9e5edae213ff
rhunter-NTatC <rhunter@newtech.coppellisd.com>
2022-09-19 12:06:56 -0500

    committ

---
+++ src/phoneList/Contact.java
@@ -0,0 +1,24 @@
+package phoneList;
+
+/*
+ * This contains info for a single contact
+ * 
+ * Parameters Needed to Store
+ *   - Contact's Name
+ *   - Contact's Phone Number
+ *   
+ * Needed Methods
+ *   - Constructor(s)
+ *   - getter methods
+ *   - Setter methods not necessary unless Constructor
+ *        initialize variables.
+ *   - Override toString() so that Contact can
+ *        be printed as part of a list.
+ * 
+ */
+
+
+
+public class Contact {
+
+}
---
+++ src/phoneList/PhoneList.java
@@ -0,0 +1,171 @@
+package phoneList;
+
+import java.util.Scanner;
+
+/*
+ * Phone List
+ * 
+ * You'll be using an ArrayList of Contact objects
+ * to crate a digital address book.
+ * 
+ * You MUST
+ *   - Use an ArrayList
+ *   - Keep list in alphabetical order
+ *   - Complete the 
+ *   
+ * You May
+ *   - Add methods as you see fit
+ *   - Change method parameters and return types
+ *   
+ * You May NOT
+ *   - change the menu method
+ *        unless you're changing what methods it calls
+ * 
+ */
+
+
+public class PhoneList {
+
+	/*
+	 * Instance Variables
+	 */
+	
+	
+	
+	//Constructor
+	public PhoneList() {
+		//initialize instance variables
+	}
+	
+	
+	/*
+	 * This should do the following
+	 * (whatever order you feel is best)
+	 *   - ask the user for contact's name and number
+	 *   - create a new Contact object with that input
+	 *   - add that object to the phone list
+	 *        in alphabetical order
+	 *        Hint: compareTo(String other)
+	 *   - tell the user what the new contact is and that 
+	 *        it's been added
+	 */
+	public static void addContact() {
+		
+		
+	}
+	
+	
+	
+	/*
+	 * This should do the following
+	 * (whatever order you feel is best)
+	 *   - ask the user for the contact's name
+	 *   - search through the list for that contact
+	 *   - if the contact is not found
+	 *        tell the user and end the method
+	 *   - if the contact is found
+	 *        Remove the item from the list
+	 *        Print the contact's name and number
+	 *        Say that contact has been removed
+	 *        Remove the item from the list
+	 *        
+	 */
+	public static void removeContact() {
+		
+		
+	}
+	
+	
+	/*
+	 * This should
+	 *   - Print all items in list
+	 *   - Must be headers for all columns
+	 *   - print in alphabetical order
+	 *   - print line of something to "box" the list
+	 *       Examples:
+	 *          *****************
+	 *          -----------------
+	 *          #################
+	 */
+	public static void printList() {
+		
+	}
+
+	
+	
+	/*
+	 * GET FAMILIAR WITH THIS MENU!!!!
+	 * 
+	 * I will be asking you to create programs with menus.
+	 * You may do them however you like.  But here is an example
+	 * of how to manage them.
+	 * 
+	 */
+	public static void menu() {
+		
+		int input = 0;
+		
+		String menu = "-----------------------\n"
+				    + "     Contacts Menu     \n"
+				    + "-----------------------\n"
+				    + "  1 - Add Contact      \n"
+				    + "  2 - Remove Contact   \n"
+				    + "  3 - Print List       \n"
+				    + "  4 - Quit             \n\n"
+				    + "Enter Choice: ";
+		
+		while (input != 4) {  //keep looping till user want's to quit
+			
+			//try getting an input
+			try {
+				String inputString = getString("\n" + menu);  //get input
+				input = Integer.valueOf(inputString);  //try converting to int
+			}
+			catch (Exception e) {
+				//if bad input, set input to 0
+				input = 0;  
+			}
+		
+			switch (input) {
+			case 1:
+				//handle menu line 1: Add Contact
+				addContact();
+				break;
+			case 2:
+				//handle menu line 2: Remove Contact
+				removeContact();
+				
+				break;
+			case 3:
+				//handle menu line 3: Print List
+				printList();
+				break;
+			case 4:
+				//handle menu line 4: Quit
+				System.out.println("\nGodbye!");
+				break;
+			default:
+				System.out.println("\nNot a valid input.\n"
+								 + "Please try again.");
+			}
+			
+			
+		
+		}
+		
+	}
+	
+	public static String getString(String str) {
+		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print(str);  //notice it's NOT a print line.  This way input is next to question.
+		return inKey.nextLine();
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		PhoneList app = new PhoneList();
+		app.menu();
+	}
+}
